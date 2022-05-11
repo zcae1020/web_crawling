f294f71fb6a5e35340142a2b9dc5a7e8174bc980
zcae1020 <zcae1020@naver.com>
2022-05-11 14:21:32 +0900

    download imgs that are crawled (complete)

--- getExamImage/examCrawling.py
+++ getExamImage/examCrawling.py
@@ -1,6 +1,14 @@
 import requests
-import urllib
+from urllib.request import urlopen
+import os
 from bs4 import BeautifulSoup
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
 
 res = requests.get('https://peakedu.tistory.com/62')
 
@@ -8,9 +16,21 @@ res = requests.get('https://peakedu.tistory.com/62')
 
 soup = BeautifulSoup(res.content, "html.parser")
 
-aset=soup.select('div.tt_article_useless_p_margin p > a')
+aset=soup.select('div.tt_article_useless_p_margin > p > a')
 
 for a in aset:
     r = requests.get(a.attrs["href"])
     s = BeautifulSoup(r.content, "html.parser")
-    print(s.find('title'))
+    #print(s.select('title'))
+    title = s.select_one('title')
+    createFolder('./img/'+ title.text)
+    imgset = s.select('figure.imageblock span > img'); 
+    n = 1
+    for img in imgset:
+        imgUrl = img['src'] 
+        with urlopen(imgUrl) as f:
+            with open('./img/' + title.text + '/' + title.text + str(n) +'.jpg','wb') as h:
+                img = f.read()
+                h.write(img)
+        n += 1
+    print('다운로드 완료')
